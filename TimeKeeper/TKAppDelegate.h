320d03e481f9427bc1d3c800a3f6c09f20197761
Vivek Yadav <mail.viveky@gmail.com>
2014-07-07 10:40:14 -0700

    Started new project: first commit

---
+++ TimeKeeper/TKAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TKAppDelegate.h
+//  TimeKeeper
+//
+//  Created by Vivek Yadav on 7/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TKAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TimeKeeper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TimeKeeper
+//
+//  Created by Vivek Yadav on 7/7/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TKAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TKAppDelegate class]));
+    }
+}
